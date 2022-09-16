d2552bc86b70d53c5410646e48d4dfda8e7acb8c
lucygoodie <79559998+lucygoodie@users.noreply.github.com>
2022-09-16 18:38:50 +1200

    init commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,19 +5,22 @@ import thunk from 'redux-thunk';
 import userReducer from './reducers/user_reducer';
 import { friendsReducer } from './reducers/friends_reducer';
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+// Unfortunately I haven't figured out how to properly implement persistence of store state yet,
+// so I have just commented out the persistence code below
 
-const persistConfig = {
-    key: 'root',
-    storage: AsyncStorage,
-}
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+// import { persistStore, persistReducer } from 'redux-persist'
+// import storage from 'redux-persist/lib/storage'
 
-const persistedReducer = persistReducer(persistConfig, userReducer);
+// const persistConfig = {
+//     key: 'root',
+//     storage: AsyncStorage,
+// }
 
-const rootReducer = combineReducers({ userReducer, friendsReducer, persistedReducer});
+// const persistedReducer = persistReducer(persistConfig, userReducer);
+
+const rootReducer = combineReducers({ userReducer, friendsReducer}); //, persistedReducer});
 
 export const Store = createStore(rootReducer, applyMiddleware(thunk));
-export const persistor = persistStore(Store);
+// export const persistor = persistStore(Store);
 
